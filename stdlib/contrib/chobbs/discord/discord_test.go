48e6a210c66c78ec6dc64267170f6e5a48cce956
chobbs <craig.hobbs@gmail.com>
2020-05-14 09:42:22 -0700

    discord

--- stdlib/contrib/chobbs/discord/discord_test.go
+++ stdlib/contrib/chobbs/discord/discord_test.go
@@ -1,238 +1,87 @@
 package discord_test
 
 import (
+	"bytes"
 	"context"
-	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
-	"sync"
 	"testing"
-	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/influxdata/flux"
 	_ "github.com/influxdata/flux/builtin"
-	_ "github.com/influxdata/flux/csv"
-	"github.com/influxdata/flux/dependencies/dependenciestest"
-	"github.com/influxdata/flux/lang"
-	"github.com/influxdata/flux/memory"
+	"github.com/influxdata/flux/dependencies/url"
 	"github.com/influxdata/flux/runtime"
 )
 
 func TestDiscord(t *testing.T) {
-	ctx := dependenciestest.Default().Inject(context.Background())
-	_, scope, err := runtime.Eval(ctx, `
-import "csv"
-import "slack"
-
-option url = "http://fakeurl.com/fakeyfake"
-option token = "faketoken"
-
-data = "
-#datatype,string,string,string
-#group,false,false,false
-#default,_result,,
-,result,qchannel,qtext,qcolor
-,,fakeChannel,this is a lot of text yay,\"#FF0000\"
-"
-
-process = slack.endpoint(url:url, token:token)( mapFn:
-	(r) => {
-		return {channel:r.qchannel,text:r.qtext,color:r.color}
-	}
-)
-
-csv.from(csv:data) |> process()
-
-`)
-
-	if err != nil {
-		t.Error(err)
-	}
-	_ = scope
-}
-
-type Server struct {
-	mu       sync.Mutex
-	ts       *httptest.Server
-	URL      string
-	requests []Request
-	closed   bool
-}
-
-func NewServer(t *testing.T) *Server {
-	s := new(Server)
+	var req *http.Request
+	var body []byte
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		sr := Request{
-			URL:           r.URL.String(), // r.URL.String(),
-			Authorization: r.Header.Get("Authorization"),
-		}
-		dec := json.NewDecoder(r.Body)
-		err := dec.Decode(&sr.PostData)
+		req = r
+		var err error
+		body, err = ioutil.ReadAll(r.Body)
 		if err != nil {
-			t.Error(err)
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
 		}
-		s.mu.Lock()
-		s.requests = append(s.requests, sr)
-		s.mu.Unlock()
+		w.WriteHeader(204)
 	}))
-	s.ts = ts
-	s.URL = ts.URL
-	return s
-}
-func (s *Server) Requests() []Request {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	return s.requests
-}
-func (s *Server) Close() {
-	if s.closed {
-		return
+	defer ts.Close()
+
+	script := fmt.Sprintf(`
+import "discord"
+import "internal/testutil"
+
+status = discord.send(url:"%s/path/a/b/c", headers: {x:"a",y:"b",z:"c"}, data: bytes(v: "body"))
+status == 204 or testutil.fail()
+`, ts.URL)
+
+	ctx := flux.NewDefaultDependencies().Inject(context.Background())
+	if _, _, err := runtime.Eval(ctx, script); err != nil {
+		t.Fatal("evaluation of http.post failed: ", err)
 	}
-	s.closed = true
-	s.ts.Close()
-}
-
-type Request struct {
-	URL           string
-	Authorization string
-	PostData      PostData
-}
-
-type PostData struct {
-	Channel     string       `json:"channel"`
-	Text        string       `json:"text"`
-	Attachments []Attachment `json:"attachments"`
-}
-
-type Attachment struct {
-	Color    string   `json:"color"`
-	Text     string   `json:"text"`
-	MrkdwnIn []string `json:"mrkdwn_in"`
-}
-
-func TestSlackPost(t *testing.T) {
-
-	s := NewServer(t)
-	defer s.Close()
-
-	testCases := []struct {
-		name    string
-		color   string
-		text    string
-		channel string
-		URL     string
-		token   string
-	}{
-		{
-			name:    "....",
-			color:   `warning`,
-			text:    "aaaaaaab",
-			channel: "general",
-			URL:     s.URL,
-			token:   "faketoken",
-		},
-		{
-			name:    "....",
-			color:   `#ffffff`,
-			text:    "qaaaaaaab",
-			channel: "general",
-			URL:     s.URL,
-			token:   "faketoken",
-		},
+	if want, got := "/path/a/b/c", req.URL.Path; want != got {
+		t.Errorf("unexpected url want: %q got: %q", want, got)
+	}
+	if want, got := "POST", req.Method; want != got {
+		t.Errorf("unexpected method want: %q got: %q", want, got)
+	}
+	header := make(http.Header)
+	header.Set("x", "a")
+	header.Set("y", "b")
+	header.Set("z", "c")
+	header.Set("Accept-Encoding", "gzip")
+	header.Set("Content-Length", "4")
+	header.Set("User-Agent", "Go-http-client/1.1")
+	if !cmp.Equal(header, req.Header) {
+		t.Errorf("unexpected header -want/+got\n%s", cmp.Diff(header, req.Header))
 	}
 
-	for _, tc := range testCases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-
-			fluxString := `import "csv"
-import "slack"
-
-endpoint = slack.endpoint(url:url, token:token)(mapFn: (r) => {
-	return {channel:r.qchannel,text:r.qtext,color:r.wcolor}
-})
-
-csv.from(csv:data) |> endpoint()`
-			extern := `
-url = "` + tc.URL + `"
-token = "` + tc.token + `"
-data = "
-#datatype,string,string,string,string,string
-#group,false,false,false,false,false
-#default,_result,,,,
-,result,,qchannel,qtext,wcolor
-,,,` + strings.Join([]string{tc.channel, tc.text, tc.color}, ",") + `"`
-			extHdl, err := runtime.Default.Parse(extern)
-			if err != nil {
-				t.Fatal(err)
-			}
-			prog, err := lang.Compile(fluxString, runtime.Default, time.Now(), lang.WithExtern(extHdl))
-			if err != nil {
-				t.Fatal(err)
-			}
-			ctx := flux.NewDefaultDependencies().Inject(context.Background())
-			query, err := prog.Start(ctx, &memory.Allocator{})
-
-			if err != nil {
-				t.Fatal(err)
-			}
-			res := <-query.Results()
-			_ = res
-			var HasSent bool
-			err = res.Tables().Do(func(table flux.Table) error {
-				return table.Do(func(reader flux.ColReader) error {
-					if reader == nil {
-						return nil
-					}
-					for i, meta := range reader.Cols() {
-						if meta.Label == "_sent" {
-							HasSent = true
-							if v := reader.Strings(i).Value(0); string(v) != "true" {
-								t.Fatalf("expecting _sent=true but got _sent=%v", string(v))
-							}
-						}
-					}
-					return nil
-				})
-			})
-			if !HasSent {
-				t.Fatal("expected a _sent column but didnt get one")
-			}
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			query.Done()
-			if err := query.Err(); err != nil {
-				t.Error(err)
-			}
-			reqs := s.Requests()
-
-			if len(reqs) < 1 {
-				t.Fatal("received no requests")
-			}
-			req := reqs[len(reqs)-1]
-
-			if req.Authorization != "Bearer "+tc.token {
-				t.Errorf("token incorrect got %s, expected %s", req.Authorization, "Bearer "+tc.token)
-			}
-			if len(req.PostData.Attachments) != 1 {
-				t.Fatalf("expected 1 attachment got %d", len(req.PostData.Attachments))
-			}
-			if req.PostData.Attachments[0].Text != tc.text {
-				t.Errorf(" got %s, expected text of %s", req.PostData.Attachments[0].Text, tc.text)
-			}
-			if req.PostData.Channel != tc.channel {
-				t.Errorf("got channel: %s, expected %s", req.PostData.Channel, tc.channel)
-			}
-			if len(req.PostData.Attachments[0].MrkdwnIn) != 0 && req.PostData.Attachments[0].MrkdwnIn[0] != "text" {
-				t.Errorf("mrkdwn_in field incorrect, should be lenth 1 with a string text in a json array")
-			}
-			if req.PostData.Attachments[0].Color != tc.color {
-				t.Errorf("got color %s, expected %s", req.PostData.Attachments[0].Color, tc.color)
-			}
-		})
+	expBody := []byte("body")
+	if !bytes.Equal(body, expBody) {
+		t.Errorf("unexpected body want: %q got: %q", string(expBody), string(body))
 	}
+}
 
+func TestDiscord_ValidationFail(t *testing.T) {
+	script := `
+import "discord"
+discord.send(url:"http://127.1.1.1/path/a/b/c", headers: {x:"a",y:"b",z:"c"}, data: bytes(v: "body"))
+`
+
+	deps := flux.NewDefaultDependencies()
+	deps.Deps.HTTPClient = http.DefaultClient
+	deps.Deps.URLValidator = url.PrivateIPValidator{}
+	ctx := deps.Inject(context.Background())
+	_, _, err := runtime.Eval(ctx, script)
+	if err == nil {
+		t.Fatal("expected failure")
+	}
+	if !strings.Contains(err.Error(), "url is not valid") {
+		t.Errorf("unexpected cause of failure, got err: %v", err)
+	}
 }
